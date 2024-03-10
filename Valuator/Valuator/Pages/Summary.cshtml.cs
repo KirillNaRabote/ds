d0cc91d70e86b5b34bc75602ff0297d237da1a31
KirillNaRabote <kirillznaet@mail.ru>
2024-03-11 01:07:08 +0300

    первую лабу добавил

---
+++ Valuator/Valuator/Pages/Summary.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
+
+namespace Valuator.Pages;
+public class SummaryModel : PageModel
+{
+    private readonly ILogger<SummaryModel> _logger;
+    private readonly IDatabase _db;
+    private readonly IConnectionMultiplexer _redis;
+
+    public SummaryModel(ILogger<SummaryModel> logger, IConnectionMultiplexer redis)
+    {
+        _logger = logger;
+        _redis = redis;
+        _db = redis.GetDatabase();
+    }
+
+    public double Rank { get; set; }
+    public double Similarity { get; set; }
+
+    public void OnGet(string id)
+    {
+        _logger.LogDebug(id);
+
+        Rank = double.Parse(_db.StringGet("RANK-" + id));
+        Similarity = double.Parse(_db.StringGet("SIMILARITY-" + id));
+    }
+}
